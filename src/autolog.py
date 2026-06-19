a3a6d690c03ae45d8c6b5835a292cdde2ff293fa
Sanikak99 <sanikaprasadkamble@gmail.com>
2026-06-20 02:14:28 +0530

    Updated till mlflow autolog

---
+++ src/autolog.py
@@ -0,0 +1,52 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+wine = load_wine()
+X = wine.data
+y = wine.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+
+max_depth = 10
+n_estimators = 5
+
+# mention your new experiment that created in the mlflow ui or below like mention experiment_id =
+#mlflow.set_experiment('MLflow_exp')  
+
+mlflow.autolog()
+
+with mlflow.start_run(experiment_id=1):
+    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=42)
+    rf.fit(X_train, y_train)
+
+    y_pred = rf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    
+    #Creating confusion matrix
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=wine.target_names, yticklabels=wine.target_names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')
+
+    plt.savefig("confusion_matrix.png")
+   
+    mlflow.log_artifact(__file__) #this is not done by autolog
+
+    # add tag
+    mlflow.set_tags({"Author": "Sanika", "Project": "Wine Classification"}) #this is tags not log so autolog also not able to do this
+
+
+
+    print(accuracy)
